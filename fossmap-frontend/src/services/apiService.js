1e10cc1e473847b37fb066354c9a387f04357db8
Vishal K <korada.vishal.phe22@itbhu.ac.in>
2024-01-28 00:04:27 +0530

    Added Client

--- fossmap-frontend/src/services/apiService.js
+++
@@ -1,9 +0,0 @@
-export async function fetchRoute(start, end) {
-    try {
-        const response = await fetch(`http://localhost:5000/api/route?start=${start}&end=${end}`);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
